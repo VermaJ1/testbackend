9499f106802713b67ba2303add050194f8741f52
Gyanendra Verma <phaltukam2@gmail.com>
2026-07-10 22:11:44 +0530

    done

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
+const cors = require('cors');
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const dns = require('dns')
 dns.setServers(['8.8.8.8','1.1.1.1'])
 
+app.use(cors());
 // Middleware
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
@@ -36,3 +39,9 @@ mongoose
         console.error(" MongoDB connection error:", err);
         process.exit(1);
     });
+    app.get("/health", (req, res) => {
+    res.json({
+        success: true,
+        message: "Backend is running"
+    });
+});
